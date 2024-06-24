b4d7cb057790d5ff9554aa2c6cd7b5fc3f6e0151
chriscupit <155696220+chriscupit@users.noreply.github.com>
2024-06-24 09:37:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function Spa() {
     return (
       <HashRouter>
         <NavBar/>
-        <UserContext.Provider value={{users:[{name:'Chris',email:'email@email.com',password:'12345',balance:'100'}]}}>              
+        <UserContext.Provider value={{users:[{name:'Chris',email:'email@email.com',password:'12345',balance:'0'}]}}>              
         <div className="container" style={{padding: "20px"}}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
